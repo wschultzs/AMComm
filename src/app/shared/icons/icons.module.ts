21939df1c7dddef0d1014099b27801dcc1a6c0b7
wschultzs <william.schultz.silva@gmail.com>
2023-02-09 12:48:27 -0300

    feat: finalizado o componente de cards

--- src/app/shared/icons/icons.module.ts
+++ src/app/shared/icons/icons.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BootstrapIconsModule } from 'ng-bootstrap-icons';
-import { PersonFill, CheckCircle, Bell } from 'ng-bootstrap-icons/icons';
+import { PersonFill, CheckCircle, Bell, CreditCard } from 'ng-bootstrap-icons/icons';
 
 const icons = {
   PersonFill,
   CheckCircle,
-  Bell
+  Bell,
+  CreditCard
 };
 
 @NgModule({
